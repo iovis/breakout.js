bcc7ec8ed1c77c99dab92340707e6ef2b4ba3f77
David Marchante <davidmarchan@gmail.com>
2017-12-03 02:05:02 +0100

    Ball moving

---
+++ index.js
@@ -0,0 +1,27 @@
+const canvas = document.getElementById('game');
+const ctx = canvas.getContext('2d');
+
+let x = canvas.width / 2;
+let y = canvas.height - 30;
+let dx = 2;
+let dy = -2;
+
+function drawBall() {
+  ctx.beginPath();
+  ctx.arc(x, y, 10, 0, Math.PI*2);
+  ctx.fillStyle = '#dabeed';
+  ctx.fill();
+  ctx.closePath();
+}
+
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawBall();
+
+  x += dx;
+  y += dy;
+
+  requestAnimationFrame(draw);
+}
+
+requestAnimationFrame(draw);
