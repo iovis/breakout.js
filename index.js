994b0c866919e479c7748d9396f1a47bd31b8ceb
David Marchante <davidmarchan@gmail.com>
2017-12-03 02:24:04 +0100

    Paddle

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const canvas = document.getElementById('game');
 const ctx = canvas.getContext('2d');
 
+// Ball
+const ballRadius = 10;
 let x = canvas.width / 2;
 let y = canvas.height - 30;
 let dx = 2;
 let dy = -2;
-
-let ballRadius = 10;
 
 function drawBall() {
   ctx.beginPath();
@@ -19,6 +19,7 @@ function drawBall() {
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawBall();
+  drawPaddle();
 
   // Collision
   if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
@@ -29,6 +30,13 @@ function draw() {
     dy = -dy;
   }
 
+  // Paddle movement
+  if (rightPressed && paddleX < canvas.width-paddleWidth) {
+    paddleX += 7;
+  } else if (leftPressed && paddleX > 0) {
+    paddleX -= 7;
+  }
+
   x += dx;
   y += dy;
 
@@ -36,3 +44,38 @@ function draw() {
 }
 
 requestAnimationFrame(draw);
+
+// Paddle
+const paddleHeight = 10;
+const paddleWidth = 75;
+
+let paddleX = (canvas.width - paddleWidth) / 2;
+let rightPressed = false;
+let leftPressed = false;
+
+function drawPaddle() {
+  ctx.beginPath();
+  ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+  ctx.fillStyle = '#dabeed';
+  ctx.fill();
+  ctx.closePath();
+}
+
+document.addEventListener('keydown', keyDownHandler, false);
+document.addEventListener('keyup', keyUpHandler, false);
+
+function keyDownHandler(e) {
+  if (e.keyCode == 39) {
+    rightPressed = true;
+  } else if (e.keyCode == 37) {
+    leftPressed = true;
+  }
+}
+
+function keyUpHandler(e) {
+  if (e.keyCode == 39) {
+    rightPressed = false;
+  } else if (e.keyCode == 37) {
+    leftPressed = false;
+  }
+}
